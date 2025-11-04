b378e61c583d0f34f9ac8d4d0a2366deeb60f57f
Matheus Ferraz <contatomatheus.oferraz@gmail.com>
2025-11-04 00:58:03 +0000

    Desafio nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -2,31 +2,51 @@
 
 // Desafio de Xadrez - MateCheck
 
-int main() {
+    // Movimentação da torre com recursividade:
+    void moverTorre(int casas){
+        if(casas > 0){
+        printf("Torre moveu-se uma casa à direita\n");
+        moverTorre(casas - 1 );
+        }
+    }
     
-    int torre = 0;
-    int rainha = 0;
-    int bispo = 0;
-
-    // Movimentação da torre com loop for:
-    for ( torre < 5; torre ++;) {
-        printf("Torre moveu-se uma casa à direita.\n"); 
-    }
-
-
-    // Movimentação do bispo com loop while:
-    while ( rainha < 8) {
-
+    // Movimentação do bispo com recursividade:
+    void moverRainha(int casas){
+        if(casas > 0){
         printf("Rainha moveu-se uma casa à esquerda.\n");
-
-        rainha ++;
+        moverRainha(casas - 1);
+        }
     }
 
-    // Movimentação bispo loop do while:
-    do {
-        printf("Bispo moveu-se uma casa para cima e uma para direita.\n");
-        bispo ++;
-    } while (bispo < 5);
+    // Movimentação bispo com recursividade:
+    void moverBispo(int i, int j){
+        for (int i = 0; i < 5; i++) {
+            printf("Bispo moveu-se uma casa para cima\n");
+        for (int j = 0; j < 1; j++) {
+            printf("Bispo moveu-se uma casa para a direita\n");
+        }
+        }
+    }
+    
+    // Movimentação do cavalo com loop aninhado:
+    void moverCavalo(int cima, int direita){
+        
+        for (int i = 0; i < 1; i++) {
+        
+        for (int j = 0; j < cima; j++) {
+            printf("Cavalo moveu-se uma casa à cima\n");
+        }
+        
+        for (int k = 0; k < direita; k++) {
+            printf("Cavalo moveu-se uma casa à direita\n");
+        }
+    }
+}; 
 
-    return 0;
+    int main() {
+        moverTorre(5);
+        moverRainha(8);
+        moverBispo(5,5);
+        moverCavalo(2,1);
+        return 0;
 }
